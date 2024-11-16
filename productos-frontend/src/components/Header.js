406a41e79f3429c501d58ca8e50f782df4cc3d6f
BastianNoMas <bastian.ihh@gmail.com>
2024-11-16 00:11:52 -0300

    Header
    Modificacion de botones

--- productos-frontend/src/components/Header.js
+++ productos-frontend/src/components/Header.js
@@ -13,6 +13,20 @@ const Header = () => {
             className="logo"
           />
         </Link>
+        <div className="button-container">
+          <Link to="/agregar" className="home-button">
+            Agregar Producto
+          </Link>
+          <Link to="/listar" className="home-button">
+            Lista de Productos
+          </Link>
+          <Link to="/editar/1" className="home-button">
+            Editar Producto
+          </Link>
+          <Link to="/crearcuenta" className="home-button">
+            Detalle Producto
+          </Link>
+        </div>
       </div>
     </header>
   );
