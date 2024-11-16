935d5373962f70b7041a6b1a885d2fdfe4cf3da8
BastianNoMas <bastian.ihh@gmail.com>
2024-11-15 23:49:06 -0300

    Revert "cambios en el front"
    This reverts commit 4c6eb14c2749cda5585d22358872a235323d8795.

--- productos-frontend/src/components/Home.js
+++ productos-frontend/src/components/Home.js
@@ -5,10 +5,6 @@ import "./Home.css"; // Para estilos personalizados
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="banner">
-        {" "}
-        <img src="/banner.jpeg" alt="Banner" className="home-banner" />
-      </div>
       <div className="button-box">
         <Link to="/agregar" className="home-button">
           Agregar Producto
