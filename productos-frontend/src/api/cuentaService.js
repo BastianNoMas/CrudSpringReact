c52e3284b8ae991f560773b47fb5a56095214e83
BastianNoMas <bastian.ihh@gmail.com>
2024-11-15 22:30:27 -0300

    Modificacion

--- productos-frontend/src/api/cuentaService.js
+++ productos-frontend/src/api/cuentaService.js
@@ -5,22 +5,8 @@ const API_URL = "http://localhost:8080/api/auth"; // Ajusta la URL si es diferen
 // Crear una nueva cuenta
 export const createCuenta = async (cuentaData) => {
   try {
-    // Realizamos la solicitud POST al backend para crear la cuenta
-    const response = await fetch("/api/cuentas", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(cuentaData),
-    });
-
-    // Si la respuesta no es exitosa, lanzamos un error
-    if (!response.ok) {
-      throw new Error("Error al crear la cuenta. Intente de nuevo.");
-    }
-
-    // Retornamos la respuesta convertida a JSON
-    return await response.json();
+    const response = await axios.post(`${API_URL}/register`, cuentaData);
+    return response.data; // Retorna la respuesta del backend
   } catch (error) {
     // Maneja errores de respuesta del servidor o de red
     throw new Error(error.response?.data || "Error al crear la cuenta");
